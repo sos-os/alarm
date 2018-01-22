6d696c173ece939b5bad74fee9d37e4f254be60e
Eliza Weisman <eliza@buoyant.io>
2018-01-21 16:35:50 -0800

    feat(base::frame::Allocator): Add frame allocator trait

--- alarm_base/src/frame.rs
+++ alarm_base/src/frame.rs
@@ -1,1 +1,29 @@
 //! Base types for page frame allocators.
+use alloc::allocator::AllocErr;
+
+/// An allocator that provides page frames.
+pub unsafe trait Allocator {
+
+    /// Architecture-dependent size of a physical page.
+    const FRAME_SIZE: usize;
+
+    /// Type representing frames provided by this allocator.
+    ///
+    /// A `Frame` must either be a pointer to a contiguous block of `FRAME_SIZE`
+    /// bytes, or be a handle that may be converted into such a pointer.
+    type Frame;
+
+    /// Returns a new `Frame`.
+    unsafe fn alloc(&mut self) -> Result<Self::Frame, AllocErr>;
+
+    /// Deallocate a `Frame`.
+    ///
+    /// # Unsafety
+    /// This function is unsafe because undefined behaviour may result if the
+    /// given `frame` was not originally allocated by this `Allocator`.
+    unsafe fn deallocate(&mut self, frame: Self::Frame);
+
+    // TODO: alloc_range/dealloc_range; requires an architecture-independent
+    //       way of representing frame ranges.
+
+}
--- alarm_base/src/lib.rs
+++ alarm_base/src/lib.rs
@@ -1,5 +1,13 @@
 //! Base types for ALARM allocators
 //!
-#![no_std]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
+#![feature(alloc, allocator_api)]
+
+extern crate alloc;
+#[cfg(feature = "std")]
+extern crate core;
+
 pub mod frame;
+
+pub use self::frame::Allocator as FrameAllocator;
