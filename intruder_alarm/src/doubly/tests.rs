5a20e9d0e0cd1c08e772b78b4041a5858a508657
Eliza Weisman <eliza@buoyant.io>
2018-01-28 11:14:02 -0800

    test(intruder_alarm::doubly): add tests for doubly-linked list of `UnsafeRef`s.

--- intruder_alarm/src/doubly/tests.rs
+++ intruder_alarm/src/doubly/tests.rs
@@ -4,7 +4,7 @@
 //  Copyright (c) 2015-2017 Eliza Weisman
 //  Released under the terms of the MIT license. See `LICENSE` in the root
 //  directory of this repository for more information.
-// 
+//
 
 use super::*;
 use super::Linked;
@@ -16,8 +16,6 @@ pub struct NumberedNode {
     pub number: usize,
     links: Links<NumberedNode>,
 }
-
-pub type NumberedList = List<usize, NumberedNode, Box<NumberedNode>>;
 
 impl NumberedNode {
     pub fn new(number: usize) -> Self {
@@ -73,6 +71,9 @@ impl Into<usize> for NumberedNode {
 mod boxed {
     use super::*;
     use std::boxed::Box;
+
+    pub type NumberedList = List<usize, NumberedNode, Box<NumberedNode>>;
+
 
     mod push_node {
         use super::*;
@@ -397,6 +398,345 @@ mod boxed {
     fn test_fromiter() {
         let list_a = (0..10).into_iter();
         let mut nlist = NumberedList::from_iter(list_a);
+
+        for i in 0..10 {
+            assert_eq!(nlist.pop_front().unwrap(), i);
+        }
+    }
+}
+
+
+
+mod unsafe_ref {
+    use super::*;
+    use UnsafeRef;
+
+    pub type UnsafeList = List<usize, NumberedNode, UnsafeRef<NumberedNode>>;
+
+
+    mod push_node {
+        use super::*;
+        use UnsafeRef;
+
+        #[test]
+        fn not_empty_after_first_push() {
+            let mut list = UnsafeList::new();
+
+            assert_eq!(list.head(), None);
+            assert_eq!(list.tail(), None);
+            assert!(list.is_empty());
+            assert_eq!(list.len(), 0);
+
+            list.push_front_node(UnsafeRef::boxed(NumberedNode::new(1)));
+
+            assert_eq!(list.is_empty(), false);
+            assert_eq!(list.len(), 1);
+        }
+
+        #[test]
+        fn contents_after_first_push() {
+            let mut list = UnsafeList::new();
+            assert_eq!(list.head(), None);
+            assert_eq!(list.tail(), None);
+
+            list.push_front_node(UnsafeRef::boxed(NumberedNode::new(555)));
+
+            assert_eq!(list.tail().unwrap().number, 555);
+            assert_eq!(list.head().unwrap().number, 555);
+        }
+
+        #[test]
+        fn head_tail_equal_after_first_push() {
+            let mut list = UnsafeList::new();
+            assert_eq!(list.head(), list.tail());
+
+            list.push_front_node(UnsafeRef::boxed(NumberedNode::new(444)));
+
+            assert_eq!(list.head(), list.tail());
+        }
+
+        #[test]
+        fn head_tail_not_equal_after_second_push() {
+            let mut list = UnsafeList::new();
+
+            list.push_front_node(UnsafeRef::boxed(NumberedNode::new(444)));
+            list.push_front_node(UnsafeRef::boxed(NumberedNode::new(555)));
+
+            assert!(list.head().unwrap() != list.tail().unwrap());
+        }
+    }
+
+    #[test]
+    fn head_tail_not_same_second_push() {
+        let mut list = UnsafeList::new();
+        let a = 444;
+        let b = 555;
+
+        list.push_front(a);
+        list.push_front(b);
+
+        assert!(list.head().unwrap() != list.tail().unwrap());
+    }
+
+    quickcheck! {
+        fn push_front_node_order(x: usize, xs: Vec<usize>) -> TestResult {
+            let mut list = UnsafeList::new();
+            list.push_front_node(UnsafeRef::boxed(NumberedNode::new(x)));
+            let mut result = TestResult::passed();
+            for x_2 in xs {
+                list.push_front_node(UnsafeRef::boxed(NumberedNode::new(x_2)));
+                result = TestResult::from_bool(
+                    list.tail().unwrap().number == x &&
+                    list.head().unwrap().number == x_2
+                );
+            }
+            result
+        }
+
+        fn not_empty_after_push(n: usize) -> bool {
+            let mut list = UnsafeList::new();
+
+            assert_eq!(list.head(), None);
+            assert_eq!(list.tail(), None);
+
+            assert!(list.is_empty());
+            assert_eq!(list.len(), 0);
+
+            list.push_front(n);
+
+            !list.is_empty() && list.len() == 1
+        }
+
+        fn contents_after_first_push(n: usize) -> bool {
+            let mut list = UnsafeList::new();
+            assert_eq!(list.head(), None);
+            assert_eq!(list.tail(), None);
+            list.push_front(n);
+            list.tail().unwrap().number == n &&
+            list.head().unwrap().number == n
+        }
+
+        fn linked_peek_prev_next(a: usize, b: usize) -> bool {
+            let mut list = UnsafeList::new();
+
+            list.push_back(a);
+            list.push_back(b);
+
+            list.head().unwrap().peek_prev() == None
+            && list.head().unwrap().peek_next() == Some(&b)
+            && list.tail().unwrap().peek_prev() == Some(&a)
+            && list.tail().unwrap().peek_next() == None
+        }
+
+        fn extend_sum_len(ys: Vec<usize>, xs: Vec<usize>) -> bool {
+            let mut list = UnsafeList::new();
+            let total = ys.len() + xs.len();
+            for y in ys {
+                list.push_back(y);
+            }
+            list.extend(xs);
+
+            list.len() == total
+        }
+
+        fn from_iter_len(xs: Vec<usize>) -> bool {
+            let lx = xs.len();
+            let list = UnsafeList::from_iter(xs);
+
+            list.len() == lx
+        }
+    }
+
+    #[test]
+    fn contents_after_push_nodes() {
+        let mut list = UnsafeList::new();
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(0)));
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(1)));
+
+        assert_eq!(list.tail().unwrap().number, 0);
+        assert_eq!(list.head().unwrap().number, 1);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(2)));
+        assert_eq!(list.tail().unwrap().number, 2);
+        assert_eq!(list.head().unwrap().number, 1);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(3)));
+        assert_eq!(list.tail().unwrap().number, 3);
+        assert_eq!(list.head().unwrap().number, 1);
+
+        assert!(!list.is_empty());
+    }
+
+    #[test]
+    fn test_pop_front_node() {
+        let mut list = UnsafeList::new();
+
+        assert_eq!(list.head(), None);
+        assert_eq!(list.tail(), None);
+        assert!(list.is_empty());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(2)));
+
+        assert!(!list.is_empty());
+        assert_eq!(list.head(), list.tail());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(1)));
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(0)));
+
+        assert_eq!(list.head().unwrap().number, 0);
+        assert_eq!(list.tail().unwrap().number, 2);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(3)));
+        assert_eq!(list.tail().unwrap().number, 3);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(4)));
+        assert_eq!(list.tail().unwrap().number, 4);
+
+        assert!(!list.is_empty());
+
+        assert_eq!(list.pop_front_node().unwrap().number, 0);
+        assert_eq!(list.pop_front_node().unwrap().number, 1);
+        assert_eq!(list.pop_front_node().unwrap().number, 2);
+        assert_eq!(list.pop_front_node().unwrap().number, 3);
+        assert_eq!(list.pop_front_node().unwrap().number, 4);
+
+        assert!(list.is_empty());
+        assert_eq!(list.pop_front_node(), None);
+    }
+
+    #[test]
+    fn test_pop_back_node() {
+        let mut list = UnsafeList::new();
+
+        assert_eq!(list.head(), None);
+        assert_eq!(list.tail(), None);
+        assert!(list.is_empty());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(2)));
+
+        assert!(!list.is_empty());
+        assert_eq!(list.head(), list.tail());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(1)));
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(0)));
+
+        assert_eq!(list.head().unwrap().number, 0);
+        assert_eq!(list.tail().unwrap().number, 2);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(3)));
+        assert_eq!(list.tail().unwrap().number, 3);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(4)));
+        assert_eq!(list.tail().unwrap().number, 4);
+
+        assert!(!list.is_empty());
+
+        assert_eq!(list.pop_back_node().unwrap().number, 4);
+        assert_eq!(list.pop_back_node().unwrap().number, 3);
+        assert_eq!(list.pop_back_node().unwrap().number, 2);
+        assert_eq!(list.pop_back_node().unwrap().number, 1);
+        assert_eq!(list.pop_back_node().unwrap().number, 0);
+
+        assert!(list.is_empty());
+        assert_eq!(list.pop_back_node(), None);
+    }
+
+    #[test]
+    fn test_pop_front() {
+        let mut list = UnsafeList::new();
+
+        assert_eq!(list.head(), None);
+        assert_eq!(list.tail(), None);
+        assert!(list.is_empty());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(2)));
+
+        assert!(!list.is_empty());
+        assert_eq!(list.head(), list.tail());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(1)));
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(0)));
+
+        assert_eq!(list.head().unwrap().number, 0);
+        assert_eq!(list.tail().unwrap().number, 2);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(3)));
+        assert_eq!(list.tail().unwrap().number, 3);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(4)));
+        assert_eq!(list.tail().unwrap().number, 4);
+
+        assert!(!list.is_empty());
+
+        assert_eq!(list.pop_front().unwrap(), 0);
+        assert_eq!(list.pop_front().unwrap(), 1);
+        assert_eq!(list.pop_front().unwrap(), 2);
+        assert_eq!(list.pop_front().unwrap(), 3);
+        assert_eq!(list.pop_front().unwrap(), 4);
+
+        assert!(list.is_empty());
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_pop_back() {
+        let mut list = UnsafeList::new();
+
+        assert_eq!(list.head(), None);
+        assert_eq!(list.tail(), None);
+        assert!(list.is_empty());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(2)));
+
+        assert!(!list.is_empty());
+        assert_eq!(list.head(), list.tail());
+
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(1)));
+        list.push_front_node(UnsafeRef::boxed(NumberedNode::new(0)));
+
+        assert_eq!(list.head().unwrap().number, 0);
+        assert_eq!(list.tail().unwrap().number, 2);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(3)));
+        assert_eq!(list.tail().unwrap().number, 3);
+
+        list.push_back_node(UnsafeRef::boxed(NumberedNode::new(4)));
+        assert_eq!(list.tail().unwrap().number, 4);
+
+        assert!(!list.is_empty());
+
+        assert_eq!(list.pop_back().unwrap(), 4);
+        assert_eq!(list.pop_back().unwrap(), 3);
+        assert_eq!(list.pop_back().unwrap(), 2);
+        assert_eq!(list.pop_back().unwrap(), 1);
+        assert_eq!(list.pop_back().unwrap(), 0);
+
+        assert!(list.is_empty());
+        assert_eq!(list.pop_back(), None);
+    }
+
+    #[test]
+    fn test_extend() {
+        let mut list = UnsafeList::new();
+
+        list.push_back(0);
+        list.push_back(1);
+
+        assert_eq!(list.tail().unwrap().number, 1);
+        assert_eq!(list.head().unwrap().number, 0);
+
+        let ext = vec![3, 4];
+        list.extend(ext);
+
+        assert_eq!(list.tail().unwrap().number, 4);
+        assert_eq!(list.head().unwrap().number, 0);
+    }
+
+    #[test]
+    fn test_fromiter() {
+        let list_a = (0..10).into_iter();
+        let mut nlist = UnsafeList::from_iter(list_a);
 
         for i in 0..10 {
             assert_eq!(nlist.pop_front().unwrap(), i);
