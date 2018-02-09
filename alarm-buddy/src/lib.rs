67149d4e0ad268b35ddfdff3a39a2ad73c9bdf2c
Eliza Weisman <eliza@buoyant.io>
2018-02-09 14:09:05 -0800

    feat(alarm-buddy): add buddy-block allocator crate

---
+++ alarm-buddy/src/lib.rs
@@ -0,0 +1,16 @@
+// ••• ALARM: the SOS memory allocator
+// --- by Eliza Weisman (eliza@elizas.website)
+// ••• and the SOS contributors
+//
+//  Copyright (c) 2018 Eliza Weisman
+//  Released under the terms of the MIT license. See `LICENSE` in the root
+//  directory of this repository for more information.
+//
+//! ALARM Buddy-Block Allocator
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
