e9443506136c17771b2acc5f8ec17e5aee9716eb
Eliza Weisman <eliza@buoyant.io>
2018-02-16 14:53:35 -0800

    feat(alarm-buddy::Heap): add `block_size` function

--- alarm-buddy/src/lib.rs
+++ alarm-buddy/src/lib.rs
@@ -18,13 +18,17 @@ use alarm_base::FrameAllocator;
 use intruder_alarm::list::{List, Linked, Links};
 use intruder_alarm::UnsafeRef;
 
+use alloc::allocator::{AllocErr, Layout};
+
 pub type FreeList = List<FreeBlock, FreeBlock, UnsafeRef<FreeBlock>>;
 
 /// A free block header.
-pub type FreeBlock = Links<FreeBlock>;
+pub struct FreeBlock {
+    links: Links<FreeBlock>
+}
 
 /// A buddy-block allocator.
-pub struct Heap<'a, F> {
+pub struct Heap<'a, F: 'a> {
 
     /// The heap's minimum block size.
     pub min_block_size: usize,
@@ -33,6 +37,8 @@ pub struct Heap<'a, F> {
     ///
     /// We cache this to avoid re-calculating.
     min_block_size_log2: usize,
+
+    /// The order of
 
     /// The number of blocks in the heap.
     ///
@@ -43,7 +49,7 @@ pub struct Heap<'a, F> {
     free_lists: &'a mut [FreeList],
 
     /// The underlying frame provider.
-    frames: &'a mut F>,
+    frames: &'a mut F,
 
 }
 
@@ -58,6 +64,55 @@ impl<'a, F> Heap<'a, F> {
 
 }
 
+impl<'a, F> Heap<'a, F>
+where
+    F: FrameAllocator
+{
+
+    /// Computes the size of an allocation request.
+    ///
+    /// # Arguments
+    /// + `layout`: the `Layout` to compute the size for.
+    pub fn block_size(&self, layout: &Layout) -> Result<usize, AllocErr> {
+        let align = layout.align();
+
+        // We cannot allocate layouts whose alignments are not powers of 2.
+        if !align.is_power_of_two() {
+            Err(AllocErr::Unsupported {
+                details: "Unsupported alignment (not a power of 2)."
+            })?;
+        }
+
+        // We cannot allocate layouts with alignments greater than the
+        // heap's base alignment (the size of a frame in the underlying
+        // frame provider).
+        if align > F::FRAME_SIZE {
+            Err(AllocErr::Unsupported {
+                details: "Unsupported alignment (exceeded FRAME_SIZE)."
+            })?;
+        }
+
+        // The allocation's size must be at least as large as the layout's
+        // alignment.
+        let size = usize::max(layout.size(), layout.align());
+
+        // Round up to the heap's minimum block size.
+        let size = usize::max(size, self.min_block_size);
+
+        // Round up to the next power oftwo.
+        let size = size.next_power_of_two();
+
+        // If the resulting size of the allocation is greater than the
+        // size of the heap,we (obviously) cannot allocate the request.
+        if size > self.heap_size {
+            Err(AllocErr::Unsupported {
+                details: "Unsupported size for alignment (exceeded heap size)."
+            })?;
+        }
+
+        Ok(size)
+    }
+}
 
 
 // ===== impl FreeBlock =====
@@ -65,12 +120,12 @@ impl<'a, F> Heap<'a, F> {
 impl Linked for FreeBlock {
     #[inline]
     fn links(&self) -> &Links<Self> {
-        &self
+        &self.links
     }
 
     #[inline]
     fn links_mut(&mut self) -> &mut Links<Self> {
-        &mut self
+        &mut self.links
     }
 }
 
